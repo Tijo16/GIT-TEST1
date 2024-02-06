7623ebab6ac4d0464c4b4b2a1e02237c95ac9b0c
tijo16 <tijotijo16@gmail.com>
2024-02-06 12:12:41 +0530

    commit

---
+++ sub.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+    public class sub
+    {
+        public static void main(String[] args)
+        {
+            int a=15,b=6;
+            System.out.println(a-b);
+        }
+    }
+
+    
