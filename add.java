55c12ce17bc4bad706b966c2fcb0dbecc4253058
tijo16 <tijotijo16@gmail.com>
2024-02-06 11:49:02 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+    public class add
+    {
+        public static void main(String[] args)
+        {
+            int a=5,b=6;
+            System.out.println(a+b);
+        }
+    }
+
+    
